321741860022a37642f01c797fa734dc1529e151
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-22 04:57:33 -0800

    add styles

--- src/component/ChatBubbles/index.jsx
+++ src/component/ChatBubbles/index.jsx
@@ -1,21 +1,33 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
-import PropTypes from "prop-types"
-import styles from "./styles.module.css"
+import PropTypes from "prop-types";
+import styles from "./styles.module.css";
 
 const ChatBubble = ({ message, origin }) => {
-    const direction = origin === "bot" ? "left" : "right";
-  
-    return (
+  const direction = origin === "bot" ? "left" : "right";
+
+  return (
+    <>
+    <div className={styles.errorMessage}>
+        <span className={styles.errorMessagemessage}>error message ...</span>
+      </div>
+    <div className={styles.messageBox}>
+        <span className={styles.messageBoxText}>. . . loading</span>
+      </div>
+
+    
+
       <div className={`${styles.container} ${styles[direction]}`}>
         <div className={styles.bubbleContent}>{message}</div>
       </div>
-    );
-  };
+    </>
+  );
+};
 
 export default ChatBubble;
 
 ChatBubble.propTypes = {
-    message: PropTypes.string,
-    origin: PropTypes.string
-}
+  message: PropTypes.string,
+  origin: PropTypes.string,
+};
--- src/component/ChatInput/index.jsx
+++ src/component/ChatInput/index.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import PropTypes from "prop-types"
-import styles from './styles.module.css';
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.module.css";
 
 const ChatInput = ({ onSendMessage }) => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -11,31 +12,41 @@ const ChatInput = ({ onSendMessage }) => {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    if (inputValue.trim() !== '') {
+    if (inputValue.trim() !== "") {
       onSendMessage(inputValue);
-      setInputValue('');
+      setInputValue("");
     }
   };
 
   return (
-    <div className={styles.container}>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          className={styles.input}
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-        <button className={styles.button}>
-          <span className={styles.buttonText}>Send</span>
-        </button>
-      </form>
-    </div>
+    <>
+{/* <div className={styles.errorMessage}>
+        <span className={styles.errorMessagemessage}>error message ...</span>
+      </div>
+
+      <div className={styles.messageBox}>
+        <span className={styles.messageBoxText}>. . . loading</span>
+      </div> */}
+
+      <div className={styles.container}>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className={styles.input}
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button className={styles.button}>
+            <span className={styles.buttonText}>Send</span>
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
 ChatInput.propTypes = {
-  onSendMessage: PropTypes.func
-}
+  onSendMessage: PropTypes.func,
+};
 
 export default ChatInput;
